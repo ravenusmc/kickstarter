19533893e3ae7e0f8ef0e3dd73ec84151f9749fb
ravenusmc <mcuddy77@gmail.com>
2020-02-14 14:22:43 -0500

    added header to about page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
+    <Header/>
     <router-view/>
   </div>
 </template>
+
+<script>
+import Header from '@/components/generic/Header.vue'
+
+export default {
+  components: {
+    Header,
+  }
+}
+</script>
 
 <style>
 </style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,13 @@
 <template>
   <div>
-    <Header />
+    <h1>Home</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Header from '@/components/generic/Header.vue'
 
 export default {
   name: 'Home',
-  components: {
-    Header,
-  }
 }
 </script>
