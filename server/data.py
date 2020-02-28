e6fff2581eb00f00de64106942d05fcd9604e80a
ravenusmc <mcuddy77@gmail.com>
2020-02-28 14:29:47 -0500

    set up data file to start working with data

---
+++ server/data.py
@@ -0,0 +1,15 @@
+#This file will deal with getting the data for the project.
+
+#importing supporting libraries
+import numpy as np
+import pandas as pd
+import datetime
+
+class Data():
+
+    def __init__(self):
+        self.data = pd.read_csv('./data/ks-projects-201801.csv')
+
+
+# data = Data()
+# data.test()
