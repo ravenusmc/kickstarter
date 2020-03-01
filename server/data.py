92e3bfd0e127d84f24602af40581b9aa2fdcd16e
ravenusmc <mcuddy77@gmail.com>
2020-03-01 11:39:44 -0500

    minor changes

--- server/data.py
+++ server/data.py
@@ -1,10 +1,10 @@
-#This file will deal with getting the data for the project.
-
 #importing supporting libraries
 import numpy as np
 import pandas as pd
 import datetime
 
+# This class will deal with a majority of the data processing that this project will
+# handle.
 class Data():
 
     def __init__(self):
