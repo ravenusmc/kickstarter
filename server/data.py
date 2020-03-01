8ddcd6eece4d8e736e69934f45516a1431fc20b0
ravenusmc <mcuddy77@gmail.com>
2020-02-29 19:01:43 -0500

    minor change

--- server/data.py
+++ server/data.py
@@ -10,6 +10,9 @@ class Data():
     def __init__(self):
         self.data = pd.read_csv('./data/ks-projects-201801.csv')
 
+    def first_chart(self):
+        pass
+
 
 # data = Data()
 # data.test()
