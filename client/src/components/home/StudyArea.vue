c5a1e85fb3ec498cfe91031847f9161d7dfe7fa4
ravenusmc <mcuddy77@gmail.com>
2020-03-02 15:04:43 -0500

    started work on form area

--- client/src/components/home/StudyArea.vue
+++ client/src/components/home/StudyArea.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    Study Area
+    <Form/>
   </div>
 </template>
 
 <script scoped>
+import Form from '@/components/forms/Form.vue';
+
 export default {
   name: 'StudyArea',
+  components: {
+    Form
+  }
 }
 </script>
 
