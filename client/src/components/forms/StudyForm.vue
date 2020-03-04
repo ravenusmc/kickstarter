3981f8220a5294aa99a5631ccbd2e203c54fd8d0
ravenusmc <mcuddy77@gmail.com>
2020-03-04 14:17:45 -0500

    set up initial form

--- client/src/components/forms/StudyForm.vue
+++ client/src/components/forms/StudyForm.vue
@@ -3,13 +3,38 @@
 
     <form @submit="submitForm">
 
-      <div class="">
+      <div>
         <select v-model="category" name="category">
           <option v-for="category in categories" v-bind:key="category" :value="category">
             {{ category }}
           </option>
         </select>
       </div>
+
+      <div>
+        <select v-model="state" name="state">
+          <option v-for="state in states" v-bind:key="state" :value="state">
+            {{ state }}
+          </option>
+        </select>
+      </div>
+
+      <div class='datePicker'>
+        <h3>Enter the Date:</h3>
+        <label>Start Date:</label>
+        <input v-model="startDate">
+        <label>End Date:</label>
+        <input  v-model="endDate">
+      </div>
+
+      <div>
+        <input v-model="moneyGoal">
+      </div>
+
+      <div>
+        <input v-model="backers">
+      </div>
+
 
       <div class='selectionFix'>
         <button>Submit</button>
@@ -21,25 +46,40 @@
 </template>
 
 <script>
+import moment from 'moment';
+
 export default {
   name: "StudyForm",
   data() {
     return {
+      startDate: '05/03/2009',
+      endDate: '02/17/2017',
       category: 'Publishing',
       categories: ['Publishing', 'Film & Video', 'Music', 'Food', 'Design', 'Crafts', 'Games',
       'Comics', 'Fashion', 'Theater', 'Art', 'Photography', 'Technology', 'Dance',
       'Journalism'],
+      state: 'successful',
+      states: ['failed', 'successful'],
+      moneyGoal: 0,
+      backers: 0,
     }
   },
   methods: {
     submitForm(evt) {
       evt.preventDefault();
-      console.log(this.category)
-      // const payload = {
-      //   question: this.question,
-      //   answer: this.answer,
-      //   sex: this.sex,
-      // };
+      this.startDate = moment(this.startDate).format('M/D/YYYY h:mm:ss A');
+      this.endDate = moment(this.endDate).format('M/D/YYYY h:mm:ss A');
+      const payload = {
+        category: this.category,
+        state: this.state,
+        currency: 'USD',
+        country: 'US',
+        startDate: this.startDate,
+        endDate: this.endDate,
+        moneyGoal: this.moneyGoal,
+        backers: this.backers,
+      };
+      console.log(payload)
       // this.fetchMapData({ payload });
     }
   }
@@ -47,6 +87,11 @@ export default {
 </script>
 
 <style scoped>
+
+form {
+  border: 2px solid red;
+  margin-bottom: 50px;
+}
 
 .selectionFix {
   margin-top: 20px;
