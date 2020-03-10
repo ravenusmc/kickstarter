3782c1ed105b5e40de95520f1ad4ac54892095c6
ravenusmc <mcuddy77@gmail.com>
2020-03-10 16:18:58 -0400

    first chart is working

--- client/src/components/home/StudyArea.vue
+++ client/src/components/home/StudyArea.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
     <StudyForm />
+    <GraphCard
+     :typeOne='typeOne'
+     :data='firstChartData'
+     :options='chartOptionsOne'>
+     </GraphCard>
   </div>
 </template>
 
 <script scoped>
+import { mapGetters } from 'vuex';
 import StudyForm from '@/components/forms/StudyForm.vue';
 import GraphCard from '@/components/charts/GraphCard.vue';
 
@@ -12,7 +18,28 @@ export default {
   name: 'StudyArea',
   components: {
     StudyForm,
+    GraphCard,
   },
+  data() {
+    return {
+      typeOne: 'BarChart',
+      chartOptionsOne: {
+        title: 'First Chart',
+        legend: { position: 'top' },
+        height: 500,
+        vAxis: {
+          viewWindow: {
+            min: 0,
+          },
+        },
+      },
+    }
+  },
+  computed: {
+  ...mapGetters([
+    'firstChartData',
+  ]),
+},
 }
 </script>
 
