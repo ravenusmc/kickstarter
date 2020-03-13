411b390a802e9378520a4adc05ac75b751675a41
ravenusmc <mcuddy77@gmail.com>
2020-03-13 14:29:45 -0400

    getting ready for second graph area

--- client/src/components/home/StudyArea.vue
+++ client/src/components/home/StudyArea.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <StudyForm />
-
+    
+    <!-- First Chart Area -->
     <div class='graphExplanationArea'>
 
       <GraphCard
@@ -26,8 +27,35 @@
          </p>
        </div>
 
+    </div>
+
+    <!-- Second Chart Area -->
+    <div class='graphExplanationArea'>
+
+      <GraphCard
+       :typeOne='typeOne'
+       :data='firstChartData'
+       :options='chartOptionsOne'>
+       </GraphCard>
+
+       <div class='paragraphDiv'>
+         <h2 class='center title'>Brief Explanation</h2>
+         <p class='paragraphFormatting'>
+           The purpose of the first chart is to show the user the number of charts
+           that have failed, succeeded or been cancelled in a given time frame. The
+           user can also select a money goal that they want to go up to as well as
+           the number of backers who want to support a project.
+         </p>
+         <p class='paragraphFormatting'>
+           The reason why I created this chart is that I wanted the users to get
+           just a basic information from the kickstarter data that I have. I also
+           want to say that some of the filters from above will not be used to build
+           this first chart.
+         </p>
+       </div>
 
     </div>
+
   </div>
 </template>
 
